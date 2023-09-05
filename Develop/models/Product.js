c83b6d85eac601c2f6e1b534f5f3deeb3374c046
BohoBabe <slenorovitz@gmail.com>
2023-09-05 16:23:14 -0700

    updated server.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -22,17 +22,24 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.DECIMAL,
       allowNull: false,
-      validate: DECIMAL
+      validate: {isDecimal: true}
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      validate: NUMERIC
+      validate: {isNumeric: true}
 
     },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
 
   },
   {
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -12,27 +12,21 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  through: {
-    model: Product
-  },
-});
+  foreignKey: 'category_id',
+  });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
+  through: ProductTag, 
+    foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
+  through: Product,
+    foreignKey: 'tag_id',
   },
-
-});
+);
 
 module.exports = {
   Product,
